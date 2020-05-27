95c4014e8e137e11d2b87099d60548382d157419
Jay Lux Ferro <jayluxferro@sperixlabs.org>
2020-05-27 07:45:44 +0000

    updated icon

--- MACAddressTool/Form1.Designer.cs
+++ MACAddressTool/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.RandomButton = new System.Windows.Forms.Button();
             this.CurrentMacTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -149,6 +150,7 @@
             this.Controls.Add(this.CurrentMacTextBox);
             this.Controls.Add(this.RandomButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
